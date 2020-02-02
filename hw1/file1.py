8d0541aa8024ebfee6af5cb55f5c3a2c9af922c2
Noureldin Hendy <nhendy@purdue.edu>
2020-02-02 15:50:52 -0500

    Nhendy/no property funcs (#1)
    * removed getters and setters
    
    * add child class
    
    * added callabe feat
    
    * WIP
    
    * fix order and use random.choice

--- hw1/file1.py
+++ hw1/file1.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 import sys
 import random
-from string import ascii_letters
-import functools
-from collections.abc import Iterable
+from string import ascii_lowercase
 
 # Fixed seed to init the RNG
 SEED = 0
 NUM_NAMES = 10
 NAME_LENGTH = 5
+WEALTH_RANGE = (0, 1000)
 
 random.seed(SEED)
 
 
 def _make_random_string(length):
-    rand_char = lambda: ascii_letters[random.randint(0, length - 1) % len(
-        ascii_letters)]
-    return ''.join([rand_char() for i in range(length)])
+    return ''.join([random.choice(ascii_lowercase) for i in range(length)])
 
 
 def _make_random_strings(num_strings, length):
@@ -48,6 +45,10 @@ class People:
             yield self._build_name(self.first_names[i], self.middle_names[i],
                                    self.last_names[i])
 
+    def __call__(self):
+        for name in sorted(self.last_names):
+            print(name)
+
     def _validate_names(self):
         if not (len(self.first_names) == len(self.middle_names) == len(
                 self.last_names)):
@@ -77,48 +78,52 @@ class People:
         self.last_name_first = False
         self.last_name_with_comma_first = False
 
-    @property
-    def first_name_first(self):
-        return self.first_name_first
 
-    @property
-    def last_name_first(self):
-        return self.last_name_first
+class PeopleWithMoney(People):
+    def __init__(self):
+        super(PeopleWithMoney, self).__init__()
+        self.wealth = [random.randint(*WEALTH_RANGE) for x in range(NUM_NAMES)]
 
-    @property
-    def last_name_with_comma_first(self):
-        return self.last_name_with_comma_first
+    def __iter__(self):
+        for name, wealth in zip(
+                super(PeopleWithMoney, self).__iter__(), self.wealth):
+            yield "{} {}".format(name, wealth)
 
-    @first_name_first.setter
-    def first_name_first(self, name):
-        self._reset_order_flags()
-        self.first_name_first = name
-
-    @last_name_first.setter
-    def last_name_first(self, name):
-        self._reset_order_flags()
-        self.first_name_first = name
-
-    @last_name_with_comma_first.setter
-    def last_name_first(self, name):
-        self._reset_order_flags()
-        self.last_name_with_comma_first = name
+    def __call__(self):
+        names_with_wealth = [n.split() for n in self.__iter__()]
+        names_with_wealth_sorted = sorted(
+            names_with_wealth,
+            key=lambda name_with_wealth: int(name_with_wealth[-1]))
+        for name_wealth in names_with_wealth_sorted:
+            print(' '.join(name_wealth))
 
 
 def _test_name_order(pple):
     for attr in [
-            'last_name_with_comma_first', 'last_name_first', 'first_name_first'
+            'first_name_first', 'last_name_first', 'last_name_with_comma_first'
     ]:
+        pple._reset_order_flags()
         setattr(pple, attr, True)
         for person in pple:
             print(person)
 
 
+def _test_callable_func(pple):
+    pple()
+
+
 def main():
     pple = People()
+    pple_w_money = PeopleWithMoney()
     for attr in ['first_names', 'middle_names', 'last_names']:
         setattr(pple, attr, _make_random_strings(NUM_NAMES, NAME_LENGTH))
+        setattr(pple_w_money, attr,
+                _make_random_strings(NUM_NAMES, NAME_LENGTH))
     _test_name_order(pple)
+    _test_callable_func(pple)
+    for per in pple_w_money:
+        print(per)
+    _test_callable_func(pple_w_money)
 
 
 if __name__ == "__main__":
