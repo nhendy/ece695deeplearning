2913a222c6cc945a834b84a12838020fdfd50a57
nhendy <nhendy@purdue.edu>
2020-02-02 16:28:33 -0500

    adding hw2

---
+++ hw2/hw02.py
@@ -0,0 +1,16 @@
+try:
+    import torchvision
+    import torch
+except ModuleNotFoundError as e:
+    print("Failed to import torchvision and torch")
+    raise
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
